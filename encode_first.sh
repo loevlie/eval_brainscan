62917c313a401cf157885c16333259341ee422c3
Dennis Loevlie <59749099+loevlie@users.noreply.github.com>
2025-07-15 14:53:40 -0400

    Create encode_first.sh

---
+++ encode_first.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python encode_KPSC_MRI.py --encoder="ViT-B/16" --encoded_directory="/home/s625378/MRI_encoded_train_2_3_4_5_7_8_val_1_test_6" --numpy_directory="/data/TUFTS_Datasets/TUFTS_MRI_Training_Dataset_numpy"
